7147894837181fcb5ce0c4d06708ae84c199bf44
KINGBOTs1 <ds767365@gmail.com>
2023-09-27 07:04:27 +0800

    new

--- config.js
+++ config.js
@@ -11,20 +11,20 @@ import { fileURLToPath } from 'url'
 
 global.owner = [
   ['6282144323683', 'DEN', true],
-  ['6285880029379']
+  ['6282144323683']
 ] // Nomor Owner
 
 global.mods = ['6282144323683'] 
-global.prems = ['6282144323683', '6285880029379']
+global.prems = ['6282144323683', '6282144323683']
 
 // apikey
-global.btc = 'apikeylu'
+global.btc = 'PKZt99sC'
 // apikeylu di ganti menggunakan apikey yang di dapatkan di website https://api.botcahx.live. contohnya global.btc = 'nans' (contoh). cara mendapatkan apikey. masuk website > scroll ke bawah dan cari harga yang kamu mau ada juga yang free dan tekan > otomatis akan di arahkan untuk registrasi, isi data diri email dll. > kalo sudah klik profil dan di situ akan muncul apikey mu. terima kasih.
 
 global.APIKeys = { // APIKey Here
   // 'https://website': 'apikey'
   'https://api-fgmods.ddns.net': 'mhdAnan',
-  'https://api.botcahx.live': 'apikeylu'
+  'https://api.botcahx.live': 'PKZt99sC'
 }
 
 global.APIs = { // API Prefix
@@ -35,26 +35,26 @@ global.APIs = { // API Prefix
 }
 
 // Watermark
-global.nama = 'NANS OFFC' // nama owner
-global.nomor = '6285880029379' // nomor owner
-global.nans = 'NANS OFFC' // nama bot 
+global.nama = 'DEN OFFC' // nama owner
+global.nomor = '6282144323683' // nomor owner
+global.nans = 'DICODING OFFC' // nama bot 
 global.thumb = 'https://i.ibb.co/m9YqsDr/neromdv3-2.jpg' // thumbnail bot ( foto menu )
 global.dygp = 'https://chat.whatsapp.com' // link group yang ada di menu
 
 // Sticker wm
-global.packname = 'NERO MD┃ᴮᴼᵀ' 
-global.author = '@nansoffc' 
-global.fgig = 'https://www.instagram.com/nansoffc' // bebas tapi jangan kosong 
-global.fgsc = 'https://github.com/nanzone' // bebas tapi jangan kosong
-global.fgyt = 'https://nansoffc.my.id/' // bebas tapi jangan kosong
-global.fgpyp = 'https://nansoffc.my.id' // bebas tapi jangan kosong
+global.packname = 'DICODING MD┃ᴮᴼᵀ' 
+global.author = '@denisetiawan' 
+global.fgig = 'https://www.instagram.com/deni' // bebas tapi jangan kosong 
+global.fgsc = 'https://github.com/KINGXDENI' // bebas tapi jangan kosong
+global.fgyt = 'https://dicodingbot.site/' // bebas tapi jangan kosong
+global.fgpyp = 'https://dicodingbot.site' // bebas tapi jangan kosong
 global.fglog = 'https://i.ibb.co/kXw4XhL/nansoffc-logo.jpg'
 
 // Other
-global.dana = '6285880029379'
+global.dana = '6282144323683'
 global.qris = 'https://i.ibb.co/xGC5Y7j/20230712-173348.jpg'
-global.web = 'https://nansoffc.my.id'
-global.email = 'nansoffc@gmail.com'
+global.web = 'https://dicodingbot.site'
+global.email = 'dicodingbot@gmail.com'
 global.lastm = 'ꜱɪᴍᴘʟᴇ ʙᴏᴛ ᴡʜᴀᴛꜱᴀᴘᴘ ʙʏ ɴᴀɴꜱ ᴏꜰꜰᴄ'
 
 global.wait = 'Tunggu sebentar....'
